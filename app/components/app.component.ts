9efed4e5133cad299567ca5827c284e92aaf105d
Sergio <sergio@sergiolealdev.com>
2017-02-25 11:52:37 +0000

    refactoring

--- app/components/app.component.ts
+++ app/components/app.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: `<h1>Hello {{name}}</h1>`
+    styleUrls:['app.component.css'],
+    templateUrl: './app.component.html'
 })
 export class AppComponent { name = 'Angular'; }
 
