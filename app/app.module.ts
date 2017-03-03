880564124cc9ae6e7cd474b0fb694f846dfea611
Sergio Leal <sergio@sergiolealdev.com>
2017-03-03 17:31:18 +0000

    adding carousel component

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,10 +1,18 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CarouselModule } from 'ng2-bootstrap';
 import { AppComponent }  from './components/app.component';
+import { DemoCarouseBasicComponent }  from './components/carousel/carousel.component';
 
 @NgModule({
-    imports:      [ BrowserModule ],
-    declarations: [ AppComponent ],
+    imports: [
+        BrowserModule,
+        CarouselModule.forRoot()
+    ],
+    declarations: [
+        AppComponent,
+        DemoCarouseBasicComponent
+    ],
     bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ app/components/carousel/carousel.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'demo-carousel-basic',
+    template: require('./carousel.component.html')
+})
+export class DemoCarouseBasicComponent {}
+
