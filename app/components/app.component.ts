99e8169b279084dbd52bf4cfdd0bf39583d28cbd
Sergio Leal <sergio@sergiolealdev.com>
2017-03-10 17:15:36 +0000

    removing main css

--- app/components/app.component.ts
+++ app/components/app.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    styles: [require('./app.component.css')],
     template: require('./app.component.html')
 })
 export class AppComponent {
